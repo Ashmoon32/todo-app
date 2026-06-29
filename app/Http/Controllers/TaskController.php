076d7d03c69a33e33a3b18d4ac066a0201dd2c3f
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-06-29 22:23:33 +0630

    add task resource route and controller

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $tasks = Task::all();
+        return view('tasks.index', [
+            'tasks' => $tasks
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('tasks.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+       $task = new Task();
+       $task->title = $request->title;
+       $task->description = $request->description;
+       $task->save();
+       return redirect('/tasks');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task $task)
+    {
+        return view('tasks.detail', [
+            'task' => Task::findOrFail($task)
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Task $task)
+    {
+        return view('tasks.edit', compact('task'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Task $task)
+    {
+        $task->title = $request->title;
+        $task->description = $request->description;
+        $task->is_completed = $request->has('is_completed');
+        $task->save();
+        return redirect('/tasks');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task)
+    {
+        $task->delete();
+        return redirect('/tasks');
+    }
+}
